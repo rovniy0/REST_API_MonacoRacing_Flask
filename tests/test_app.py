7753126e66e8fdc1f82987b0958886c0a1fe8fe7
Grisha Rovniy <rovniygrishaaa@gmail.com>
2024-02-06 13:07:54 +0200

    add content validation tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,42 @@
 
 import pytest
+from bs4 import BeautifulSoup
+
+
+def test_report_content(client):
+    response = client.get('/report/')
+    soup = BeautifulSoup(response.data, 'html.parser')
+    h1 = soup.find('h1')
+    assert h1.text == 'Report of Monaco Racing'
+    ul = soup.find('ul')
+    assert ul is not None
+
+
+def test_report_content_with_order(client):
+    response = client.get('/report/?order=desc')
+    soup = BeautifulSoup(response.data, 'html.parser')
+    h1 = soup.find('h1')
+    assert h1.text == 'Report of Monaco Racing'
+    ul = soup.find('ul')
+    assert ul is not None
+
+
+def test_report_all_drivers_content(client):
+    response = client.get('/report/drivers/')
+    soup = BeautifulSoup(response.data, 'html.parser')
+    h1 = soup.find('h1')
+    assert h1.text == 'Report of Monaco Racing'
+    ul = soup.find('ul')
+    assert ul is not None
+
+
+def test_report_driver_id_content(client):
+    response = client.get('/report/drivers/?driver_id=DRR')
+    soup = BeautifulSoup(response.data, 'html.parser')
+    h1 = soup.find('h1')
+    assert h1.text == 'Report of Monaco Racing'
+    ul = soup.find('ul')
+    assert ul is not None
 
 
 def test_report_status_code(client):
@@ -12,7 +49,7 @@ def test_report_all_drivers_status_code(client):
     assert response.status_code == 200
 
 
-def test_report_drivers_status_code(client):
+def test_report_driver_id_status_code(client):
     response = client.get('/report/drivers/?driver_id=DRR')
     assert response.status_code == 200
 
