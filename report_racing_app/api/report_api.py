999fc516b25742a4d3f24f35843f2598a6b375df
Grisha Rovniy <rovniygrishaaa@gmail.com>
2024-02-13 16:51:00 +0200

    add error handling

--- report_racing_app/api/report_api.py
+++ report_racing_app/api/report_api.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_restful import Resource
 from report_racing_app import build_report
 from flasgger import swag_from
@@ -13,8 +13,12 @@ app = Flask(__name__)
 class GetReport(Resource):
     @swag_from('./swagger_docs/report.yml')
     def get(self):
-        request_format = request.args.get('request_format', type=str)
+        request_format = request.args.get('request_format', type=str, default='json')
         request_order = request.args.get('request_order', type=str, default='asc')
+
+        if request_order not in ['asc', 'desc'] or request_format not in ['json', 'xml']:
+            abort(400, 'Invalid request_format or request_order value')
+
         result_abbreviations, sorted_time_result = build_report('data_files')
         report_dict = {}
 
@@ -37,7 +41,11 @@ class GetReport(Resource):
 class GetDrivers(Resource):
     @swag_from('./swagger_docs/drivers.yml')
     def get(self):
-        request_format = request.args.get('request_format', type=str)
+        request_format = request.args.get('request_format', type=str, default='json')
+
+        if request_format not in ['json', 'xml']:
+            abort(400, 'Invalid request_format value')
+
         result_abbreviations, sorted_time_result = build_report('data_files')
 
         if request_format == 'xml':
@@ -50,7 +58,11 @@ class GetDrivers(Resource):
 class GetDriverById(Resource):
     @swag_from('./swagger_docs/driver_by_id.yml')
     def get(self, driver_id):
-        request_format = request.args.get('request_format', type=str)
+        request_format = request.args.get('request_format', type=str, default='json')
+
+        if request_format not in ['json', 'xml']:
+            abort(400, 'Invalid request_format value')
+
         result_abbreviations, sorted_time_result = build_report('data_files')
         report_dict = {}
 
@@ -60,6 +72,8 @@ class GetDriverById(Resource):
                 "name": result_abbreviations[driver_id],
                 "time": time_str
             }
+        else:
+            abort(400, f'Driver {driver_id} not found')
 
         if request_format == 'xml':
             xml_report = dicttoxml(report_dict)
