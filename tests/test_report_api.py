b75f76a15858484b4cef45dda14e27096075bbbf
Grisha Rovniy <rovniygrishaaa@gmail.com>
2024-02-16 01:16:41 +0200

    update all the tests

--- tests/test_report_api.py
+++ tests/test_report_api.py
@@ -4,7 +4,7 @@ import pytest
 
 class TestGetReportApiCases:
 
-    def test_get_report_incorrect_arguments(self, client):
+    def test_get_report_incorrect_arguments(self, client, db):
         response = client.get('/api/v1/report/?request_order=some_value')
         assert response.status_code == 400
         response = client.get('/api/v1/report/?request_format=some_value')
@@ -12,64 +12,64 @@ class TestGetReportApiCases:
         response = client.get('/api/v1/report/?request_format=some_value&request_order=some_value')
         assert response.status_code == 400
 
-    def test_get_report_format_json(self, client):
+    def test_get_report_format_json(self, client, db):
         response = client.get('/api/v1/report/')
         assert response.status_code == 200
-        assert (b'{"SVF": {"name": "Sebastian Vettel | '
-                b'FERRARI", "time": "0:01:04.415000"}') in response.data
+        assert (b'{"SVF":{"name":"Sebastian Vettel",'
+                b'"team":"FERRARI","time":"0:01:04.415000"}}') in response.data
 
-    def test_get_report_order_desc(self, client):
+    def test_get_report_order_desc(self, client, db):
         response = client.get('/api/v1/report/?request_order=desc')
         assert response.status_code == 200
-        assert (b'{"LHM": {"name": "Lewis Hamilton |'
-                b' MERCEDES", "time": "0:06:47.540000"}') in response.data
+        assert (b'{"SVF":{"name":"Sebastian Vettel",'
+                b'"team":"FERRARI","time":"0:01:04.415000"}}') in response.data
 
-    def test_get_report_format_xml(self, client):
+    def test_get_report_format_xml(self, client, db):
         response = client.get('/api/v1/report/?request_format=xml')
         assert response.status_code == 200
         assert (b'"<?xml version=\\"1.0\\" encoding=\\"UTF-8\\" ?><root><SVF type=\\"dict\\"'
-                b'><name type=\\"str\\">Sebastian Vettel | FERRARI</name><time type=\\"str\\">'
-                b'0:01:04.415000</time></SVF>') in response.data
+                b'><name type=\\"str\\">Sebastian Vettel</name><team type=\\"str\\">FERRARI</t'
+                b'eam><time type=\\"str\\">0:01:04.415000</time></SVF></root>"\n') in response.data
 
 
 class TestGetDriversApiCases:
 
-    def test_get_drivers_incorrect_arguments(self, client):
+    def test_get_drivers_incorrect_arguments(self, client, db):
         response = client.get('/api/v1/report/?request_format=some_value')
         assert response.status_code == 400
 
-    def test_get_drivers_format_json(self, client):
+    def test_get_drivers_format_json(self, client, db):
         response = client.get('/api/v1/report/drivers/')
         assert response.status_code == 200
-        assert b'{"BHS":"Brendon Hartley | SCUDERIA TORO ROSSO HONDA"' in response.data
+        assert b'{"SVF": {"name": "Sebastian Vettel"}}\n' in response.data
 
-    def test_get_drivers_format_xml(self, client):
+    def test_get_drivers_format_xml(self, client, db):
         response = client.get('/api/v1/report/drivers/?request_format=xml')
         assert response.status_code == 200
-        assert (b'<BHS type=\\"str\\">Brendon Har'
-                b'tley | SCUDERIA TORO ROSSO HONDA</BHS>') in response.data
+        assert (b'"<?xml version=\\"1.0\\" encoding=\\"UTF-8\\" ?><root><SVF type=\\"dict\\"'
+                b'><name type=\\"str\\">Sebastian Vettel</name></SVF></root>"\n') in response.data
 
 
 class TestGetDriverByIdApiCases:
 
-    def test_get_drivers_incorrect_arguments(self, client):
+    def test_get_drivers_incorrect_arguments(self, client, db):
         response = client.get('/api/v1/report/drivers/?request_format=some_value')
         assert response.status_code == 400
         response = client.get('/api/v1/report/drivers/AAA')
         assert response.status_code == 404
 
-    def test_get_driver_by_id_format_json(self, client):
-        response = client.get('/api/v1/report/drivers/DRR')
+    def test_get_driver_by_id_format_json(self, client, db):
+        response = client.get('/api/v1/report/drivers/SVF')
         assert response.status_code == 200
-        assert (b'{"DRR":{"name":"Daniel Ricciardo | RED BULL RACING TAG HEUER",'
-                b'"time":"0:02:47.987000"}}\n') in response.data
+        assert (b'{"SVF":{"name":"Sebastian Vettel",'
+                b'"team":"FERRARI","time":"0:01:04.415000"}}\n') in response.data
 
-    def test_get_driver_by_id_format_xml(self, client):
-        response = client.get('/api/v1/report/drivers/DRR?request_format=xml')
+    def test_get_driver_by_id_format_xml(self, client, db):
+        response = client.get('/api/v1/report/drivers/SVF?request_format=xml')
         assert response.status_code == 200
-        assert (b'"<?xml version=\\"1.0\\" encoding=\\"UTF-8\\" ?><root><DRR type=\\"dict\\"'
-                b'><name type=\\"str\\">Daniel Ricciardo | RED BULL RACING TAG HEUER</name><'
-                b'time type=\\"str\\">0:02:47.987000</time></DRR></root>"\n') in response.data
+        assert (b'"<?xml version=\\"1.0\\" encoding=\\"UTF-8\\" ?><root><SVF type=\\"dict\\"'
+                b'><name type=\\"str\\">Sebastian Vettel</name><team type=\\"str\\">FERRARI</t'
+                b'eam><time type=\\"str\\">0:01:04.415000</time></SVF></root>"\n') in response.data
 
 
 def test_swagger_status_code(client):
