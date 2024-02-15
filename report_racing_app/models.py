f0620c0ca9773c98b1b68525710ebf5922fd273e
Grisha Rovniy <rovniygrishaaa@gmail.com>
2024-02-15 17:08:21 +0200

    update Driver model

--- report_racing_app/models.py
+++ report_racing_app/models.py
@@ -1,12 +1,21 @@
 
 from peewee import *
 
+
 db = SqliteDatabase('driver.db')
 
 
-class Driver(Model):
+class BaseModel(Model):
+
+    class Meta:
+        database = db
+
+
+class Driver(BaseModel):
     id = PrimaryKeyField(unique=True)
     name = CharField()
     team = CharField()
     result_time = TimeField()
 
+
+Driver.create_table()
